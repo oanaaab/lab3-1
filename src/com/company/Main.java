2932d51f1236ba3313999e572d6bb5c4eb0efae7
Oana Balint <oanab95@gmail.com>
2016-03-26 13:14:19 +0200

    modificare la codul lui Alin

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("salut lume");
         System.out.println("salut lume 2");
-        System.out.println("salut lume 3");
+        System.out.println("salut lume 3333");
     }
 }
